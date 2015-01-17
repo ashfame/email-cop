c5c07cc2c3295dc8130298b0b81a4f9d772c0c5b
Ashfame <mail@ashfame.com>
2015-01-18 01:42:00 +0530

    change the way of showing email because HTML email just needs to be echo'ed to be able to show the design and simple text email needs a bit of styling to make them look nice

--- inc/cpt_template.php
+++ inc/cpt_template.php
@@ -1,15 +1,94 @@
+<?php
+the_post();
+$email_body = get_post( $post->ID )->post_content;
+
+// If its a complete HTML email, then we just try to show email meta information on top of the actual email
+if ( false !== strpos( $email_body, '<!DOCTYPE html>' ) ) {
+	?>
+	<style>
+		.email-meta {
+			background: #FFF;
+			padding: 25px;
+		}
+		.email-meta-content {
+			max-width: 800px;
+			margin: auto;
+		}
+		.avatar {
+			border-radius: 60px;
+			border: solid 1px #CECECE;
+			width: 100px;
+			height: 100px;
+		}
+		td {
+			padding: 5px;
+			vertical-align: top;
+		}
+		.email-meta p {
+			margin-top: 4px;
+			margin-bottom: 4px;
+		}
+	</style>
+
+	<div class="email-meta">
+		<div class="email-meta-content">
+			<table>
+				<tr>
+					<td>
+						<?php echo get_avatar( get_post_meta( $post->ID, 'wp_cop_email_from', true ), 200 ); ?>
+					</td>
+					<td>
+						<p>
+							<strong>From:</strong> <?php echo get_post_meta( $post->ID, 'wp_cop_email_from_name', true ) . ' &lt;' . get_post_meta( $post->ID, 'wp_cop_email_from', true ) . '&gt;'; ?>
+						</p>
+						<p>
+							<?php if ( is_array( get_post_meta( $post->ID, 'wp_cop_email_to', true ) ) ) { ?>
+								<strong>To:</strong> <?php echo implode( ', ', get_post_meta( $post->ID, 'wp_cop_email_to', true ) ); ?>
+							<?php } else { ?>
+								<strong>To:</strong> <?php echo get_post_meta( $post->ID, 'wp_cop_email_to', true ); ?>
+							<?php } ?>
+						</p>
+						<?php if ( get_post_meta( $post->ID, 'wp_cop_email_cc', true ) ) { ?>
+							<p>
+								<strong>Cc:</strong> <?php echo get_post_meta( $post->ID, 'wp_cop_email_cc', true ); ?>
+							</p>
+						<?php } ?>
+						<?php if ( get_post_meta( $post->ID, 'wp_cop_email_bcc', true ) ) { ?>
+							<p>
+								<strong>Bcc:</strong> <?php echo get_post_meta( $post->ID, 'wp_cop_email_bcc', true ); ?>
+							</p>
+						<?php } ?>
+						<p>
+							<strong>Subject:</strong> <?php echo get_post_meta( $post->ID, 'wp_cop_email_subject', true ); ?>
+						</p>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+
+	<?php
+	// now output email content and bail
+	echo $email_body;
+	return;
+}
+
+// if we are here that means, its a simple text email, so lets roll out complete HTML to make it look nice
+?>
 <html>
 	<head>
 		<?php wp_head(); ?>
 		<style>
 			html, body {
-				background: #CECECE;
+				background: #f2efe9;
 			}
-			#wp-cop-preview-email {
+			.email-meta {
 				background: #FFF;
-				margin: auto;
-				max-width: 800px;
 				padding: 25px;
+			}
+			.email-meta-content {
+				max-width: 800px;
+				margin: auto;
 			}
 			.avatar {
 				border-radius: 60px;
@@ -21,7 +100,7 @@
 				padding: 5px;
 				vertical-align: top;
 			}
-			.email_meta p {
+			.email-meta p {
 				margin-top: 4px;
 				margin-bottom: 4px;
 			}
@@ -29,12 +108,21 @@
 				background: #f2efe9;
 				padding: 10px;
 			}
+			.email-body-content {
+				max-width: 800px;
+				margin: auto;
+				background: #FFF;
+				padding: 10px;
+			}
+			.email-body-content p {
+				margin-bottom: 15px;
+			}
 		</style>
 	</head>
 	<body>
-		<?php the_post(); ?>
-		<div id="wp-cop-preview-email">
-			<div class="email_meta">
+
+		<div class="email-meta">
+			<div class="email-meta-content">
 				<table>
 					<tr>
 						<td>
@@ -68,7 +156,10 @@
 					</tr>
 				</table>
 			</div>
-			<div class="email-body">
+		</div>
+
+		<div class="email-body">
+			<div class="email-body-content">
 				<?php the_content(); ?>
 			</div>
 		</div>
